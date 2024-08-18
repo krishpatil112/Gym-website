48555962df822a47d48e0f9d31b616fcd0f0d60d
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-08-18 20:58:35 +0530

    store register values

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios"
 
 function Signup() {
+    const[name, setName] = useState()
+    const[email, setEmail] = useState()
+    const[password, setPassword] = useState()
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('',{name, email, password})
+        .then(result => console.log(result))
+        .catch(err=> console.log(err))
+    }
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h2>Register</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="email">
               <strong>Name</strong>
@@ -17,6 +30,7 @@ function Signup() {
               autoComplete="off"
               name="email"
               className="form-control rounded-0"
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="mb-3">
@@ -29,6 +43,7 @@ function Signup() {
               autoComplete="off"
               name="email"
               className="form-control rounded-0"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-3">
@@ -40,6 +55,7 @@ function Signup() {
               placeholder="Enter Password"
               name="password"
               className="form-control rounded-0"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button type="submit" className="btn btn-success w-100 rounded-0">
